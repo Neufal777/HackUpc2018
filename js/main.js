b07743da5d86a65f410a90293732bc1e0676956b
Naoufal Dahouli <neufal777@gmail.com>
2018-10-21 07:51:41 +0200

    FrontEnd upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
+function voted(){
 
-class Block{
-    
-    constructor(index, data, previousHash = ''){
-        this.index = index;
-        this.data = data;
-        this.date = new Date();
-        this.previousHash = previousHash;
-        
-         console.log(index);
-    }   
+	var buttonVote1 = document.getElementById('buttonVote1');
+	var buttonVote2 = document.getElementById('buttonVote2');
+	var buttonVote3 = document.getElementById('buttonVote3');
+
+	var num_of_votes1 = document.getElementById('num_of_votes1');
+	var num_of_votes2 = document.getElementById('num_of_votes2');
+	var num_of_votes3 = document.getElementById('num_of_votes3');
+
+	//we hide the buttons after the votation	
+	buttonVote1.style.display = "none";
+	buttonVote2.style.display = "none";
+	buttonVote3.style.display = "none";
+
+	//make visible the number of votes of each one
+	num_of_votes1.style.display = "block";
+	num_of_votes2.style.display = "block";
+	num_of_votes3.style.display = "block";
+
 }
-
-bloque = new Block(0,'naoufal');
-
-bloque;
